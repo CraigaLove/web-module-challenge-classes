8d7e6d2962b718423962a37a6d11bb864d5760c1
CraigaLove <craigalove0341@gmail.com>
2022-09-01 09:53:23 -0400

    first task complete

--- index.js
+++ index.js
@@ -44,9 +44,26 @@ class Airplane {
 */
 
 class Person {
-  
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+    this.stomach = [];
+  }
+  eat(edible) {
+    if(this.stomach < 10) {
+      this.stomach.push(edible);
+    }
+  }
+  poop() {
+    this.stomach = [];
+  }
+  toString() {
+    return `${this.name}, ${this.age}`;
+  }
 }
 
+const Mary = new Person('Mary', '50');
+console.log(Mary.toString());
 /*
   TASK 2
     - Write a Car class whose constructor initializes `model` and `milesPerGallon`, from 2 arguments.
