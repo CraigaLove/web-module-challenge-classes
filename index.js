816ff2ea454a26ba2a8b3355c9df3f1059f2a654
CraigaLove <craigalove0341@gmail.com>
2022-09-01 10:03:58 -0400

    second task complete

--- index.js
+++ index.js
@@ -79,8 +79,28 @@ console.log(Mary.toString());
 */
 
 class Car {
-  
+  constructor(model, milesPerGallon) {
+    this.model = model;
+    this.milesPerGallon = milesPerGallon;
+    this.tank = 0;
+    this.odometer = 0;
+  }
+  fill(gallons) {
+    this.tank = this.tank + gallons;
+  }
+  drive(distance) {
+    const maxDistance = this.tank * this.milesPerGallon;
+    if(distance <= maxDistance) {
+      this.odometer = this.odometer + distance;
+      this.tank = this.tank - (distance / this.milesPerGallon);
+    }else{
+      this.odometer = this.odometer + maxDistance;
+      this.tank = 0;
+      return `I ran out of fuel at ${this.odometer} miles!`
+    }
+  }
 }
+
 
 /*
   TASK 3
